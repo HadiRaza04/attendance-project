2a5b6abd6b970f52bc2fef7e1ff5a37ee65f3d48
HadiRaza04 <hadiraza.edu@gmail.com>
2022-11-12 18:30:03 +0500

    some-code

---
+++ app.js
@@ -0,0 +1,53 @@
+const trigger = document.querySelector('#trigger');
+const modalWrapper = document.querySelector('.modal_wrapper');
+const close = document.querySelector('#closed');
+const btn_purple2 = document.querySelector('.btn_purple2');
+trigger.addEventListener('click', function(){
+    modalWrapper.classList.add('active');
+});
+close.addEventListener('click', function(){
+    modalWrapper.classList.remove('active');
+});
+btn_purple2.addEventListener('click', function(){ 
+    modalWrapper.classList.remove('active');
+    alert('LogIn successfully');    
+});
+
+
+
+// var hidediv = document.getElementById("hideshow");
+var email = document.getElementsByTagName(input[type="email"]).value;
+var password = document.getElementsByTagName(input[type="password"]).value;
+console.log(password)
+
+
+
+
+
+function signup() {
+    
+}
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDHsnTyHSxlQxyYHU47e8xbZf1yDOfOjWU",
+  authDomain: "attendence-13c2e.firebaseapp.com",
+  projectId: "attendence-13c2e",
+  storageBucket: "attendence-13c2e.appspot.com",
+  messagingSenderId: "702283216302",
+  appId: "1:702283216302:web:8efe4d514d4841fd29443f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+
+
